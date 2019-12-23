48deae390e0d748018c336f3700a858d884852ed
paddy <padmanaban.g@prodapt.com>
2019-12-23 11:46:38 +0100

    added files

---
+++ scripts/utils.js
@@ -0,0 +1,65 @@
+
+function loadPage(id) {
+  $(document).ready(function(){	
+		$("#page").load("componentsPages/"+id+".html");	
+});
+}
+
+
+
+
+
+//Load footer and default graph
+function loadFooter(){
+
+$(document).ready(function(){
+   
+   $('#contact').load("utilPages/contact.html");
+   
+   $('#footerContent').load("utilPages/footer.html");
+   
+    $('#page').load("componentsPages/ottda.html");
+
+});
+}
+
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}
+
+
+
+var addclass = 'color';
+
+var $cols = $('.divs').click(function(e) {
+    $cols.removeClass(addclass);
+    $(this).addClass(addclass);
+});
+
+
+
+
+
+
+
+
